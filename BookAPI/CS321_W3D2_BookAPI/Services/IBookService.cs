66cdaf28ccfa3480dcbe4b6032d31cd64e206ae8
ck1962 <ckeslin@gmail.com>
2019-08-05 18:01:30 -0500

    started using CS321_W3D2_BookAPI.Models;

---
+++ BookAPI/CS321_W3D2_BookAPI/Services/IBookService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using CS321_W3D2_BookAPI.Models;
+
+namespace CS321_W3D2_BookAPI.Services
+{
+    public interface IBookService
+    {
+        // CRUDL - create (add), read (get), update, delete (remove), list
+
+        // create
+        Book Add(Book book); 
+        // read
+        Book Get(int id);
+        // update
+        Book Update(Book book); 
+        // delete
+        void Remove(Book book);
+        // list
+        IEnumerable<Book> GetAll();
+    }
+}
